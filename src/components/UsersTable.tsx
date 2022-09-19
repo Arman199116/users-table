1c96d506e5bb1f5a66fd270a2b341e6aecd0ead0
Arman199116 <arman.ochinyan@gmail.com>
2022-09-20 00:01:41 +0400

    in progress search

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -6,29 +6,34 @@ import { IUser } from "./../model";
 const UsersTable : React.FC = () => {
 
     const [sortedData, setSortedData] = useState<IUser[]>(data);
+    const [originalData, setOriginalData] = useState<IUser[]>(data);
     const [sortByDesc, setSortByDesc] = useState<boolean>(true);
+    const [searchInpVal, setSeaarchInpVal] = useState<string>('');
 
     let sortById = (e? : React.MouseEvent) : void => {
-        const clickedEl = e?.target as HTMLTableHeaderCellElement;
+        const clickedEl = e?.target as HTMLTableCellElement;
         setSortedData(sortedData.sort((a : IUser , b : IUser) => sortByDesc ? a.id - b.id : b.id - a.id ));
         setSortByDesc(prev => !prev);
         clickedEl?.classList.toggle('id-icon-dir-down');
     }
 
-    let searchByColumn = (e ?: React.ChangeEvent) : void => {
-        const changedEl = e?.target as HTMLInputElement;
+    let searchByColumn = (e : React.ChangeEvent) : void => {
+        const changedEl = e.target as HTMLInputElement;
 
         let searchedInput : string = changedEl.value?.trim().toLowerCase();
-        let getByColumn : string | undefined = changedEl.dataset['userinfo'];
-       
+        let getByColumn : string = changedEl.dataset['userinfo'] || '';
+        setSeaarchInpVal(searchedInput);
+
         if (searchedInput) {
-            //`${item}.${getByColumn}`.indexOf(searchedInput) >= 0
-            setSortedData([...sortedData.filter((user : IUser) => {
-                console.log(typeof `${user}.${getByColumn}`);
-                
-                return `${user}.${getByColumn}`.indexOf(searchedInput.toString()) >= 0
-            })]);
-        }  
+            //console.log(sortedData[0][getByColumn as keyof typeof sortedData[0]]);
+            if (searchedInput.length < 1) {
+                setSortedData(originalData)
+            } else {
+                setSortedData([...sortedData.filter((user : IUser) => {
+                    return user[getByColumn as keyof typeof user].toString().indexOf(searchedInput) >= 0
+                })]);
+            }
+        }
     }
 
     useEffect(() => {
@@ -43,11 +48,11 @@ const UsersTable : React.FC = () => {
                     <tr >
                         <th className="table-headers">
                             <p onClick={e => sortById(e) } className="id-icon-dir-top" >Id</p>
-                            <input onChange={e => searchByColumn(e)} data-userinfo='id' type="text" placeholder="Serarch by" />
+                            <input onChange={e => searchByColumn(e)}  data-userinfo='id' type="text" placeholder="Serarch by" />
                         </th>
                         <th className="table-headers">
                             <p>FirstName</p>
-                            <input data-userinfo='firstName' type="text" placeholder="Serarch by" />
+                            <input onChange={e => searchByColumn(e)}  data-userinfo='firstName' type="text" placeholder="Serarch by" />
                         </th>
                         <th className="table-headers">
                             <p>LastName</p>
@@ -64,7 +69,7 @@ const UsersTable : React.FC = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {   
+                    {
                         sortedData.map((item : IUser) => (
                             <tr key={item.id} className='users-table-row'>
                                 <td>{item.id}</td>
